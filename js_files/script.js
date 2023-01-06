2c6a2ca8a08345f2e8048a1e5fb2000eca77f614
Phillardb <pbostic789@gmail.com>
2023-01-06 09:14:29 -0500

    event listener testing

---
+++ js_files/script.js
@@ -0,0 +1,6 @@
+//nav link wrapper icons
+const navLinkBox = document.getElementsByClassName("nav-link-wrapper");
+navLinkBox[0].addEventListener("click", function(e) {
+    console.log("event");
+});
+
